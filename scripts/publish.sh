563ca613344c2bc495af02418955da21e0de7a8e
taylorjackson <taylorjackson@bc1092225.ca>
2024-08-06 17:11:31 -0700

    license

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -16,6 +16,9 @@ if [ -n "$NEW_VERSION" ] && [ "$LATEST_PUBLISHED_VERSION" != "$NEW_VERSION" ]; t
   cat ./package.json | jq '.version = $ENV.NEW_VERSION' > ./package-edited.json
   mv ./package-edited.json ./package.json
 
+  cp ./LICENSE.md ./LICENSE
+
+
   npm install
   npm run build
   npm publish --access public
